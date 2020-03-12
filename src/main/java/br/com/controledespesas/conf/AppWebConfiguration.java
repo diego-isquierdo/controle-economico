4a4d62a437d5a267099aa2a394c10261577b755f
Diego Isquierdo <diego.isquierdo@hotmail.com>
2020-03-12 12:37:58 -0300

    criado home.jsp e configura MVC para recohecer a pagina principal da aplicacao

--- src/main/java/br/com/controledespesas/conf/AppWebConfiguration.java
+++ src/main/java/br/com/controledespesas/conf/AppWebConfiguration.java
@@ -1,15 +1,19 @@
 package br.com.controledespesas.conf;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.controledespesas.controllers.HomeController;
 
 @EnableWebMvc
+@EnableCaching 
 @ComponentScan(basePackageClasses = {HomeController.class})
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Bean
 	public InternalResourceViewResolver  internalResourceViewResolver() {
@@ -19,5 +23,10 @@ public class AppWebConfiguration {
 		
 		return resolver;
 	}
+	
+	@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 	
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,12 +1,19 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+	<meta charset="ISO-8859-1">
+	<title>Insert title here</title>
+	<c:url value="/resources/css" var="cssPath" />
+	<link rel="stylesheet" href="${cssPath}/style.css" />
 </head>
 <body>
 	<h1>Hello World</h1>
+	
+	
+	
 </body>
 </html>
